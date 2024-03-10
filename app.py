a1b32c2756d38cec655665b7d0ec52882bb23c04
Locottus <herlich.gonzalez@mangopractice.com>
2024-03-10 17:50:06 -0600

    added new endpoint for qr generation without UI

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import qrcode
 from io import BytesIO
 import base64
 
 app = Flask(__name__)
+
+@app.route('/api/qr', methods=['GET'])
+def generate_qr_api():
+    url = request.args.get('url')
+    if not url:
+        return "Error: No URL provided. Please specify a URL parameter.", 400
+
+    qr_img = qrcode.make(url)
+    bytes_io = BytesIO()
+    qr_img.save(bytes_io, 'PNG')
+    bytes_io.seek(0)
+    return send_file(bytes_io, mimetype='image/png')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
